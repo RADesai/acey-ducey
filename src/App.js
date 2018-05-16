8bf3f71cdc3e5c8a324f4dae3173db74a5fc4a81
Raj Ashish Desai <rajdesai94@gmail.com>
2018-05-15 22:38:06 -0400

    Add fonts, game description

--- src/App.js
+++ src/App.js
@@ -2,17 +2,21 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import actions from './actions/actions';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Acey-Deucy</h1>
-        </header>
+      <div className="container">
+        <div className="app-title">Acey Deucy</div>
+        <div className="app-description">
+          Before the action, each player must add their ante into the pot.
+          Two cards are then dealt face-up to one player.
+          That player then bets from nothing to the amount that is in the pot at the time whether or not the third card will numerically fall in between the first two.
+          If the third card falls in between the two other cards, the bettor takes the amount he bet out of the pot;
+          if the third card falls outside of the two other cards, the bettor must add what he bet to the pot;
+          and if the third card matches the numerical value of one of the other two cards, the bettor must add to the pot double what he bet.
+        </div>
       </div>
     );
   }
--- src/App.test.js
+++ src/App.test.js
@@ -16,7 +16,11 @@ describe('App Container', () => {
     expect(appContainer.exists()).toEqual(true);
   });
   
-  it("should render app title: 'Acey-Deucy'", () => {
-    expect(appContainer.find('.App-title').text()).toEqual('Acey-Deucy');
+  it("should render app title: 'Acey Deucy'", () => {
+    expect(appContainer.find('.app-title').text()).toEqual('Acey Deucy');
+  });
+  it("should render app description", () => {
+    expect(appContainer.find('.app-description').text())
+      .toEqual(`Before the action, each player must add their ante into the pot. Two cards are then dealt face-up to one player. That player then bets from nothing to the amount that is in the pot at the time whether or not the third card will numerically fall in between the first two. If the third card falls in between the two other cards, the bettor takes the amount he bet out of the pot; if the third card falls outside of the two other cards, the bettor must add what he bet to the pot; and if the third card matches the numerical value of one of the other two cards, the bettor must add to the pot double what he bet.`);
   });
 });
