50aa44639fd7ac55a0d2943bcf76388e03571cdc
Raj Ashish Desai <rajdesai94@gmail.com>
2018-05-19 15:43:19 -0400

    prettier formatting

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,18 +1,14 @@
-import React from 'react'
-import './Card.css';
+import React from "react";
+import "./Card.css";
 
 const Card = props => {
-    const { suit, value } = props;
-    return (
-        <div className="card">
-            <div className="card-value">
-                { value }
-            </div>
-            <div className="card-suit">
-                { suit }
-            </div>
-        </div>
-    );
-}
+  const { suit, value } = props;
+  return (
+    <div className="card">
+      <div className="card-value">{value}</div>
+      <div className="card-suit">{suit}</div>
+    </div>
+  );
+};
 
 export default Card;
